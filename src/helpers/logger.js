009268aec905610857cbcb3a899c9d1f384ff687
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2021-09-20 03:16:58 +0200

    Add winston as main logger

---
+++ src/helpers/logger.js
@@ -0,0 +1,43 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  timestamp: true,
+  transports: [],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      level: 'silly',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.prettyPrint(),
+        winston.format.printf((log) => {
+          if (log instanceof Error) {
+            return `[${log.level}] : ${log.timestamp} : ${log.message} ${log.stack}`;
+          }
+          return `[${log.level}] : ${log.timestamp} : ${log.message}`;
+        })
+      ),
+      timestamp: true,
+      handleExceptions: true,
+      humanReadableUnhandledException: true,
+      exitOnError: false,
+    })
+  );
+} else {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.cli()
+      ),
+    })
+  );
+}
