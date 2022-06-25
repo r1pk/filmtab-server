e876965f07cc75e29d3772102897db27b05cb1f3
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-06-25 23:58:30 +0200

    Create new function to generate random ids
    - Create new function to generate random ids with a given length.

--- src/utils/get-unique-id.js
+++ src/utils/get-unique-id.js
@@ -1,3 +1,11 @@
-export const getUniqueId = () => {
-  return Date.now().toString(36) + Math.random().toString(36).substring(2);
+export const availableCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+export const getUniqueId = (length = 8) => {
+  let uniqueString = '';
+
+  for (let i = 0; i < length; i++) {
+    uniqueString += availableCharacters[Math.floor(Math.random() * availableCharacters.length)];
+  }
+
+  return uniqueString;
 };
